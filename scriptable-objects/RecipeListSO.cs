1c556c0c73b60bbabd97d09fb0014c5296ae24bd
Zanamoff <118935418+Zanamoff@users.noreply.github.com>
2025-05-10 14:37:24 +0500

    Add files via upload

---
+++ scriptable-objects/RecipeListSO.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+//comment this out if needed
+public class RecipeListSO : ScriptableObject {
+    public List<RecipeSO> recipeSOList;
+}
+
---
+++ scriptable-objects/RecipeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class RecipeSO : ScriptableObject {
+    public string recipeName;
+    //public Sprite recipeSprite;
+    //Implement later
+    public List<KitchenObjectSO> kitchenObjectSOList;
+
+}
+
